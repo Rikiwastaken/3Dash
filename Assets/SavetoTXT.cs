d61a007f4440b7f2f50df44a80cac7ea0ae3f013
arnaudkrafft <arnaud.krafft@telecomnancy.net>
2024-11-26 15:41:07 +0100

    state saved to persistentdatapath

---
+++ Assets/SavetoTXT.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavetoTXT : MonoBehaviour
+{
+    private Transform Ennemis;
+
+    private List<Transform> EnnemisList = new List<Transform>();
+
+    public int stringtimer;
+
+    void Start()
+    {
+        Ennemis = GameObject.Find("Enemies").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stringtimer==0)
+        {
+            SaveJSON(CreateString());
+
+            stringtimer = 5;
+        }
+        else
+        {
+            stringtimer--;
+        }
+    }
+
+    string CreateString()
+    {
+        string res = "{\n";
+        int nbchildren = Ennemis.childCount;
+        EnnemisList = new List<Transform>();
+        for (int i = 0; i < nbchildren; i++)
+        {
+            Transform t = Ennemis.GetChild(i);
+            res += "\"("+i+ ")\":{\"coordinates\":[" + t.position.x + "," + t.position.y + "," + t.position.z+"]";
+
+            if (t.GetComponent<Enemyscript>().movedirection == 1)
+            {
+                res += ",\"state\":\"x\"";
+            }
+            else if (t.GetComponent<Enemyscript>().movedirection == 1)
+            {
+                res += ",\"state\":\"y\"";
+            }
+            else
+            {
+                res += ",\"state\":\"NA\"";
+            }
+
+            res += "}";
+            if(i!=nbchildren-1)
+            {
+                res += ",";
+            }
+            res += "\n";
+        }
+        res += "}";
+        return res;
+    }
+
+    void SaveJSON(string JSONtosave)
+    {
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/Ennemis.json", JSONtosave);
+    }
+
+}
